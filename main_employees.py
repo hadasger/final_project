ca7853188393d706283b2dec379c21616d3c7ec4
hadasger <misskremer@gmail.com>
2022-09-15 23:39:35 +0300

    chack validation of input fields

--- main_employees.py
+++ main_employees.py
@@ -5,17 +5,54 @@ df_employees = pd.read_csv('./employees_data.csv')
 df_employees.head()
 
 #asking the user to type a new employee data
+
 print('Enter an employee\'s name:')
-emp_name = input()
+#check if the user entered a valid name
+while True:
+    emp_name = input()
+    if emp_name.isalpha():
+        break
+    else:
+        print ('Enter a valid employee\'s name:')
+        
 
 print(f'Enter {emp_name}\'s ID number:')
-emp_id = input()
+#check if the user entered a valid ID number
+while True:
+    emp_id = input()
+    if emp_id.isnumeric():
+        if len(emp_id)==9:
+            break
+        else:
+            print('Enter a 9 digits employee\'s ID mumber:')
+    else:
+        print ('Enter a valid employee\'s ID number:')
 
-print(f'Enter {emp_name}\'s phone number:')
-emp_phone_number = input()
+
+print(f'Enter {emp_name}\'s phone number (digits only):')
+#check if the user entered a valid phonenumber
+while True:
+    emp_phone_number = input()
+    if emp_phone_number.isnumeric():
+        if len(emp_phone_number)==10:
+            break
+        else:
+            print('Enter a 10 digits employee\'s phone mumber:')
+    else:
+        print ('Enter a valid employee\'s phone number:')
 
 print(f'Enter {emp_name}\'s age:')
-emp_age = input()
+#check if the user entered a valid age
+while True:
+    emp_age = input()
+    if emp_age.isnumeric():
+        emp_age = int(emp_age)
+        if (emp_age >= 21) and (emp_age <= 67):
+            break
+        else:
+            print('Enter a valid employee\'s age between 21 to 67:')
+    else:
+        print ('Enter a valid employee\'s age:')
 
 #add the new employee data to the employee_data file
 employee_details = [emp_id, emp_name, emp_phone_number, emp_age]

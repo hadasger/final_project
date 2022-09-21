305748f1635df7b3f8a13d51836942c7b777dc9a
hadasger <misskremer@gmail.com>
2022-09-21 23:14:31 +0300

    check validation of a file and add it to the main file

--- main_employees.py
+++ main_employees.py
@@ -4,6 +4,7 @@ import pandas as pd
 #creating file of employees data
 df_employees = pd.read_csv('./employees_data.csv')
 df_employees.head()
+# print(df_employees)
 
 
 
@@ -76,12 +77,48 @@ while True:
 employee_details = [emp_id, emp_name, emp_phone_number, emp_age]
 df_employees.loc[len(df_employees.index)] = employee_details
 
+# print(df_employees)
+
+
+
 #asking the user to type a new file of employees
 
 print('Enter a file of employees data name:')
-file_of_employees = input()
+file_of_emp = input()
+df_emp_file = pd.read_csv(file_of_emp)
+df_emp_file.head()
+# print(df_emp_file)
 
 #check how many employees/rows in the file
 
+row_num_emp_file = len(df_emp_file.axes[0])
 
+#check validation of input employees file
+valid_rows = 0
+for row in range(row_num_emp_file):
+    if validate_id_number(str(df_emp_file.loc[row]['ID'])) == True:
+            if validate_name(df_emp_file.loc[row]['Name']) == True:
+                    if validate_phone_number(str(df_emp_file.loc[row]['Phone'])) == True:
+                            if validate_age(int(df_emp_file.loc[row]['Age'])) == True: 
+                                valid_rows +=1
+                            else:
+                                print('The age column in row number:', row, 'isn\'t valid')
+                                break
+                    else:
+                        print('The phone number column in row number:', row, 'isn\'t valid')
+                        break
+            else:
+                print('The name column in row number:', row, 'isn\'t valid')
+                break
+    else:
+        print ('The ID column in row number:', row, 'isn\'t valid')
+        break
+    
 
+#add the new employees file to the employee_data file
+if valid_rows == row_num_emp_file:
+    main_employees_data = pd.concat([df_employees, df_emp_file])
+else:
+    print('Someting went wrong. We didn\'t add your file to the main data frame')
+
+print(main_employees_data)
